02c7fc31b36444beaae84b1bfea0a283d74fa1e4
David Antonio Stadnick Machado Rusycki <1020388@UNIDAVI.local>
2023-04-04 00:16:19 -0300

    modificando esquema de cache para suportar instalação desktop

--- src/html/sw.js
+++ src/html/sw.js
@@ -1,48 +1,40 @@
-var CACHE_NAME = 'static-v1';
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      return cache.addAll([
-        '/',
-        '../html/index.html',
-        '../html/catedral.html',
-        '../html/mirante-javali.html',
-        '../html/ponte-arcos.html',
-        '../html/manifest.json',
-        '../imagem/categral.png',
-        '../imagem/david.png',
-        '../imagem/icon.png',
-        '../imagem/javali.png',
-        '../imagem/ponte-arcos.png',
-        '../css/index.css',
-        '../css/pagina.css',
-        '../css/reset.css'
-      ]);
-    })
-  )
-});
-
-self.addEventListener('activate', function activator(event) {
-  event.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys
-        .filter(function (key) {
-          return key.indexOf(CACHE_NAME) !== 0;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
+//instalando o service worker
+self.addEventListener('install', function(e) {
+  console.log('Service Worker Instalado');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Adicionando dados no cache');
+      return cache.addAll(filesToCache);
     })
   );
 });
 
+//atualizar o cache
+self.addEventListener('activate', function(e) {
+  console.log('Service Worker Ativo');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('Service Worker Cache Antigo Removido', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-      caches.match(event.request).then(function (cachedResponse) {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
+
+//verificar se informação está disponivel no cache
+self.addEventListener('fetch', function(e) {
+  console.log('Service Worker Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+//retirado de https://developers.google.com/web/fundamentals/getting-started/codelabs/your-first-pwapp/?hl=pt-br
